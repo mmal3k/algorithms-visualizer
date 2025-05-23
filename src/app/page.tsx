2fa3ca61462d63f63e51efef9b179b80928d65a8
mmal3k <mmalekrais@gmail.com>
2025-05-23 17:48:32 +0200

    add header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,47 @@
+"use client";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@radix-ui/react-tabs";
+import { useState } from "react";
+import PathfindingVisualizer from "./visualizer/pathfinding/page";
+import SortingVisualizer from "./visualizer/sorting/page";
+
 export default function Home() {
+  const [activeTab, setActiveTab] = useState("sorting");
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        {"hello guys !"}
+    <div className="min-h-screen bg-background">
+      <header className="sticky top-0 z-50 w-full border-b bg-primary px-10">
+        <div className="flex h-14 items-center justify-between">
+          <div className="text-xl font-bold text-white">logo</div>
+          <Tabs
+            value={activeTab}
+            onValueChange={setActiveTab}
+            className="w-auto"
+          >
+            <TabsList className="grid w-full grid-cols-2 p-1 bg-white/20 border-white/30">
+              <TabsTrigger
+                value="sorting"
+                className="text-lg px-1 text-white data-[state=active]:bg-white data-[state=active]:text-primary"
+              >
+                Sorting
+              </TabsTrigger>
+              <TabsTrigger
+                value="pathfinding"
+                className="text-lg px-1 text-white data-[state=active]:bg-white data-[state=active]:text-primary"
+              >
+                Pathfinding
+              </TabsTrigger>
+            </TabsList>
+          </Tabs>
+        </div>
+      </header>
+      <main className="">
+        <Tabs value={activeTab} onValueChange={setActiveTab} className="h-full">
+          <TabsContent value="sorting" className="mt-0 h-full">
+            <SortingVisualizer />
+          </TabsContent>
+          <TabsContent value="pathfinding" className="mt-0 h-full">
+            <PathfindingVisualizer />
+          </TabsContent>
+        </Tabs>
       </main>
     </div>
   );
---
+++ src/app/visualizer/pathfinding/page.tsx
@@ -0,0 +1,3 @@
+export default function PathfindingVisualizer() {
+  return <div>Pathfinding Visualizer</div>;
+}
